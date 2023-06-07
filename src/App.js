455585daf6da5993d8a80954a43a7bccd2f38cc0
rukeeo1 <rukeeojigbo@gmail.com>
2023-06-07 06:48:04 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import bannerImage from './assets/banner.jpg';
 import './App.scss';
 import { ProductCard } from './components/ProductCard.js';
+
+
 const App = () => {
-  const [shoes, setShoes] = useState([{
-    "id": 1,
-    "name": "Running Shoes",
-    "brand": "Nike",
-    "price": 59.99,
-    "image": "running_shoes.jpg",
-    "description": "These running shoes provide excellent cushioning and support.",
-    "category": "Sports",
-    "shoeCategory": "Running",
-    "rating": 4.5,
-    "reviews": 10,
-    "availability": true
-}, ]);
+  const [shoes, setShoes] = useState([
+    {
+      id: 1,
+      name: 'Running Shoes',
+      brand: 'Nike',
+      price: 59.99,
+      image: 'running_shoes.jpg',
+      description:
+        'These running shoes provide excellent cushioning and support.',
+      category: 'Sports',
+      shoeCategory: 'Running',
+      rating: 4.5,
+      reviews: 10,
+      availability: true,
+    },
+  ]);
   useEffect(() => {
     const fetchShoes = async () => {
-      console.log(`${process.env.REACT_APP_API_URL}/get-shoes`)
-      fetch(`${process.env.REACT_APP_API_URL}/get-shoes`)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response, 'shoes sucessfully retrieved');
-          setShoes(response);
+      console.log(`${process.env.REACT_APP_API_URL}/get-shoes`);
+
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/get-shoes`, {
+          headers: {
+            Accept: '*/*',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':
+              'Origin, X-Requested-With, Content-Type, Accept',
+          },
         })
-        .catch((error) =>
-          console.log('something went wrong with getting shoes', error)
-        );
+        .then((response) => {
+          // Handle the successful response
+          console.log(response.data);
+          setShoes(response.data);
+        })
+        .catch((error) => {
+          // Handle the error
+          console.error(error, 'there was an error fetching data');
+        });
+
+      // fetch(`${process.env.REACT_APP_API_URL}/get-shoes`)
+      //   .then((response) => response.json())
+      //   .then((response) => {
+      //     console.log(response, 'shoes sucessfully retrieved');
+      //     setShoes(response);
+      //   })
+      //   .catch((error) =>
+      //     console.log('something went wrong with getting shoes', error)
+      //   );
     };
     fetchShoes();
   }, []);
