97dec23c74789a8bb8c4706411e6b9edb1279f7f
Willochs316 <willochs316@gmail.com>
2023-06-07 21:13:42 +0100

    improve the structure of billboard component for better readability

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import bannerImage from './assets/banner.jpg';
-import './App.scss';
-import { ProductCard } from './components/ProductCard.js';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import bannerImage from "./assets/banner.jpg";
+import "./App.scss";
+import { ProductCard } from "./components/ProductCard.js";
+import Productlist from "./components/Productlist/Productlist";
 
 const App = () => {
   const [shoes, setShoes] = useState([
     {
       id: 1,
-      name: 'Running Shoes',
-      brand: 'Nike',
+      name: "Running Shoes",
+      brand: "Nike",
       price: 59.99,
-      image: 'running_shoes.jpg',
+      image: "running_shoes.jpg",
       description:
-        'These running shoes provide excellent cushioning and support.',
-      category: 'Sports',
-      shoeCategory: 'Running',
+        "These running shoes provide excellent cushioning and support.",
+      category: "Sports",
+      shoeCategory: "Running",
       rating: 4.5,
       reviews: 10,
       availability: true,
@@ -25,39 +26,60 @@ const App = () => {
     const fetchShoes = async () => {
       // this is a test app, ignore the console.log
       console.log(`${process.env.REACT_APP_API_URL}/get-shoes`);
-
+      console.log(process.env.REACT_APP_HOST, "host");
 
       axios
         .get(`${process.env.REACT_APP_API_URL}/get-shoes`, {
           headers: {
-            'Accept': '*/*',
+            Accept: "*/*",
+            Host: process.env.REACT_APP_HOST,
+            "Accept-Encoding": "gzip, deflate, br",
+            Connection: "keep-alive",
           },
         })
         .then((response) => {
           // Handle the successful response
-          console.log(response.data,'data retrieved');
+          console.log(response.data, "data retrieved");
           setShoes(response.data);
         })
         .catch((error) => {
           // Handle the error
-          console.error(error, 'there was an error fetching data');
+          console.error(error, "there was an error fetching data");
         });
     };
     fetchShoes();
   }, []);
   return (
-    <div className='homepage'>
+    <div className="homepage">
       <div
-        className='homepage__banner'
+        className="homepage__banner"
         style={{ backgroundImage: `url(${bannerImage})` }}
       >
-        <div className='homepage__banner-overlay d-flex align-items-center pl-4'>
-          <h1 className='ml-4'>Shoes</h1>
+        {/* billboard banner content */}
+        <div className="homepage__billboard-banner-content d-flex align-items-center">
+          <div className="homepage__billboard-header-container">
+            <h2 class="homepage__banner-header">Lacoste x Roland-Garros</h2>
+
+            <div class="homepage__banner-title">Game, set and style</div>
+          </div>
+
+          <a class="homepage__category-button-him" data-type="category">
+            <span>For him</span>
+          </a>
+
+          <a class="homepage__category-button-her" data-type="category">
+            <span>For her</span>
+          </a>
         </div>
+        {/* end of billboard banner content */}
       </div>
+
+      {/* productlisting  */}
+      <Productlist />
+
       {/* a simple header comes in here */}
-      <div className='homepage__main container mt-5'>
-        {shoes.map((shoe) => (
+      <div className="homepage__main container mt-5">
+        {shoes?.shoes?.map((shoe) => (
           <ProductCard product={shoe} key={shoe.id} />
         ))}
       </div>
