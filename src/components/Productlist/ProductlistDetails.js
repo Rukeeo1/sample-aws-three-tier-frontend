516996727d7dd33b6093193ca61ef00c958d4149
Willochs316 <willochs316@gmail.com>
2023-06-08 14:12:21 +0100

    improve code structure for better organization and readability

--- src/components/Productlist/ProductlistDetails.js
+++ src/components/Productlist/ProductlistDetails.js
@@ -1,4 +1,13 @@
 import LacosteNeo from "../../assets/lacoste-neo.jpg";
+import LacosteLeather from "../../assets/lacoste-leather.jpg";
+import LacosteActive from "../../assets/lacoste-active.jpg";
+import LacosteCarnaby from "../../assets/lacoste-carnaby.jpg";
+import LacosteOdyssa from "../../assets/lacoste-odyssa.jpg";
+import LacostePique from "../../assets/lacoste-pique.jpg";
+import LacosteSpin from "../../assets/lacoste-spin.jpg";
+import LacosteTextile from "../../assets/lacoste-textile.jpg";
+import LacosteTrainers from "../../assets/lacoste-trainers.jpg";
+
 import { Svgs } from "../../assets/svgs";
 
 export const products = [
@@ -9,4 +18,60 @@ export const products = [
     numberOfColor: "+ 6 colours",
     itemFlag: "Bestseller",
   },
+  {
+    img: `${LacosteLeather}`,
+    title: "Men's Lacoste L003 Neo Trainers",
+    eye: <Svgs.Eye className="product__eye" />,
+    numberOfColor: "+ 6 colours",
+    itemFlag: "Bestseller",
+  },
+  {
+    img: `${LacosteActive}`,
+    title: "Men's Lacoste L003 Neo Trainers",
+    eye: <Svgs.Eye className="product__eye" />,
+    numberOfColor: "+ 6 colours",
+    itemFlag: "Bestseller",
+  },
+  {
+    img: `${LacosteCarnaby}`,
+    title: "Men's Lacoste L003 Neo Trainers",
+    eye: <Svgs.Eye className="product__eye" />,
+    numberOfColor: "+ 6 colours",
+    itemFlag: "Bestseller",
+  },
+//   {
+//     img: `${LacosteOdyssa}`,
+//     title: "Men's Lacoste L003 Neo Trainers",
+//     eye: <Svgs.Eye className="product__eye" />,
+//     numberOfColor: "+ 6 colours",
+//     itemFlag: "Bestseller",
+//   },
+//   {
+//     img: `${LacostePique}`,
+//     title: "Men's Lacoste L003 Neo Trainers",
+//     eye: <Svgs.Eye className="product__eye" />,
+//     numberOfColor: "+ 6 colours",
+//     itemFlag: "Bestseller",
+//   },
+//   {
+//     img: `${LacosteSpin}`,
+//     title: "Men's Lacoste L003 Neo Trainers",
+//     eye: <Svgs.Eye className="product__eye" />,
+//     numberOfColor: "+ 6 colours",
+//     itemFlag: "Bestseller",
+//   },
+//   {
+//     img: `${LacosteTextile}`,
+//     title: "Men's Lacoste L003 Neo Trainers",
+//     eye: <Svgs.Eye className="product__eye" />,
+//     numberOfColor: "+ 6 colours",
+//     itemFlag: "Bestseller",
+//   },
+//   {
+//     img: `${LacosteTrainers}`,
+//     title: "Men's Lacoste L003 Neo Trainers",
+//     eye: <Svgs.Eye className="product__eye" />,
+//     numberOfColor: "+ 6 colours",
+//     itemFlag: "Bestseller",
+//   },
 ];
