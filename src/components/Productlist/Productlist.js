c2a10bfca979ea546e77ddf8325f8299979eff8a
Willochs316 <willochs316@gmail.com>
2023-06-08 13:43:58 +0100

    improve code structure for better organization and readability

--- src/components/Productlist/Productlist.js
+++ src/components/Productlist/Productlist.js
@@ -1,7 +1,8 @@
 import React from "react";
 import LacosteNeo from "../../assets/lacoste-neo.jpg";
-import "./Productlist.css";
 import { Svgs } from "../../assets/svgs";
+import { products } from "./ProductlistDetails";
+import "./Productlist.css";
 
 const Productlist = () => {
   return (
@@ -14,45 +15,41 @@ const Productlist = () => {
 
       <div className="product__list-container">
         <div className="product__slide-list">
-          <div className="product__item-container">
-            <div className="product__image-wrapper">
-              <a href="/" className="product__image-link">
-                <div className="product__image-content">
-                  <img className="product__image" src={LacosteNeo} alt="" />
+          {products.map((product, index) => (
+            <div className="product__item-container" key={index}>
+              <div className="product__image-wrapper">
+                <a href="/" className="product__image-link">
+                  <div className="product__image-content">
+                    <img className="product__image" src={product.img} alt="" />
 
-                  <div className="product__image-pointer"></div>
-                </div>
-              </a>
-            </div>
-
-            <div className="product__title-container">
-              <div className="product__title-content">
-                <div className="product__main-title">
-                  <a className="product__main-title-link">
-                    Men's Lacoste L003 Neo Trainers
-                  </a>
-                </div>
-
-                <button
-                  className="product__btn-eye-container btn--small btn--icon"
-                  title="Quick view"
-                  data-quick-view-init="id-11"
-                >
-                  <Svgs.Eye className="product__eye" />
-                </button>
+                    <div className="product__image-pointer"></div>
+                  </div>
+                </a>
               </div>
 
-              <span class="product__number-of-color-tile flex">
-                + 6 colours
-              </span>
+              <div className="product__title-container">
+                <div className="product__title-content">
+                  <div className="product__main-title">
+                    <a className="product__main-title-link">{product.title}</a>
+                  </div>
 
-              <div class="product__title-flags">
-                <div class="product__item-flag item-flag">
-                  Bestseller
+                  <button className="product__btn-eye-container btn--small btn--icon">
+                    {product.eye}
+                  </button>
+                </div>
+
+                <span class="product__number-of-color-tile flex">
+                  {product.numberOfColor}
+                </span>
+
+                <div class="product__title-flags">
+                  <div class="product__item-flag item-flag">
+                    {product.itemFlag}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/Productlist/ProductlistDetails.js
@@ -0,0 +1,12 @@
+import LacosteNeo from "../../assets/lacoste-neo.jpg";
+import { Svgs } from "../../assets/svgs";
+
+export const products = [
+  {
+    img: `${LacosteNeo}`,
+    title: "Men's Lacoste L003 Neo Trainers",
+    eye: <Svgs.Eye className="product__eye" />,
+    numberOfColor: "+ 6 colours",
+    itemFlag: "Bestseller",
+  },
+];
